791c5bb885a57c365734d979c4b739eb2b6f984a
JOAO FELIPE GOBETI CALENZANI <2018103815@labgrad.inf.ufes.br>
2018-04-12 14:44:13 -0300

    Mudanças

--- exercicio2-Lista1.hs
+++ exercicio2-Lista1.hs
@@ -1,1 +1,15 @@
 numCedulas valor = div valor 500 + div (mod valor 500) 100 + div (mod valor 100) 50 + div (mod valor 50) 10 + div (mod valor 10) 5 + mod valor 5  
+
+areaRetangulo x y = x * y
+
+areaCirculo r = pi * (r^2)
+
+distancia x1 y1 x2 y2 = sqrt(((x2 - x1)^2) + ((y2 - y1)^2))
+
+raizP a b c = (-b + sqrt(b^2 -4*a*c))/2*a
+
+raizN a b c = (-b - sqrt(b^2 -4*a*c))/2*a
+
+raizes a b c = (raizP a b c, raizN a b c)
+
+

cb847918ae6c788fcd6d539bdfb5011cc3a34be9
João Felipe Gobeti Calenzani <joao.calen@hotmail.com>
2018-05-09 15:26:26 -0300

    modificando

--- exercicio2-Lista1.hs
+++ exercicio2-Lista1.hs
@@ -1,3 +1,7 @@
+main :: IO ()
+main = return ()
+
+
 numCedulas valor = div valor 500 + div (mod valor 500) 100 + div (mod valor 100) 50 + div (mod valor 50) 10 + div (mod valor 10) 5 + mod valor 5  
 
 areaRetangulo x y = x * y
@@ -12,18 +16,25 @@ raizN a b c = (-b - sqrt(b^2 -4*a*c))/2*a
 
 raizes a b c = (raizP a b c, raizN a b c)
 
-potenciaN a b = 1 / (a ^ (-b))
 
 potencia a b = 
-		 if (b > 0) then
-		   a ^ b
-		 else
-		 		 if((b==0) && (not(a==0))) then
-		   		1
-		 			else
-					 	 if (b < 0) then
-		   				1/(a ^ (-b))
-		 				 else
-		   				"Potencia nao pode ser realizada" 
+		if (b > 0) then
+		   potenciaP a b
+		else
+		 		if((b==0) && (not(a==0))) then
+		   			1
+		 		else
+				 	if (b < 0) then
+		   				potenciaN a b
+		 			else					 	
+		   				 imprimirErro 
+	where
+	potenciaN a b = 1 / (a ^ (-b))
+	potenciaP a b = a ^ b
+	imprimirErro =  0
 
+potencia :: Float -> Integer -> Float
+
+
+celsiusToFarenheit c = (c*1.8) + 32
 

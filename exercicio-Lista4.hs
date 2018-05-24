d0e7987b40ee77ad5ceb3ef3e5659c5ec12b07f7
JOAO FELIPE GOBETI CALENZANI <2018103815@labgrad.inf.ufes.br>
2018-05-24 14:36:27 -0300

    olar

--- exercicio-Lista4.hs
+++ exercicio-Lista4.hs
@@ -26,3 +26,24 @@ foo3 x =
         else if( x <= 50 ) then 'C'
         else if( x <= 80 ) then 'B'
     else 'A'
+
+{-- 
+
+2-
+
+I)
+
+a) [5,10..79]
+
+
+
+--}
+
+
+verificaOrdem xs = verifica xs
+                    where
+                        i <- [0.. length xs -2]
+                        verifica xs = xs!!i <= xs!!(i + 1)
+                                    
+
+verificaOrdem2 xs = [xs!!i | i <- [0.. length xs -2], xs!!i > xs!!(i+1)] == []                                        
